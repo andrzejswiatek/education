97746322f36e09bfef6c2fbdb72ae13fa8534156
Andrzej Świątek <andrzej.swiatek00@gmail.com>
2018-11-04 15:27:17 +0100

    chore: dodanie pliku Wybor.py

---
+++ lessons/Wybor.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    wybor = input("Wybierz jedna z opcji: [1,2]:")
+    print('Wybrales:{0}'.format(wybor))
+
+    if wybor == '1':
+        print('Opcja 1')
+    elif wybor == '2':
+        print('Opcja 2')
+
+main()
