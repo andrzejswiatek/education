8de6b535e15c795c3e6390aa775a0c8f52ecee78
mateuszswiatek <mateuszswiatek3@gmail.com>
2018-11-15 20:29:50 +0100

    Zapoznanie z VSC

--- lessons/wprowadzenie/Wybor.py
+++ lessons/wprowadzenie/Wybor.py
@@ -1,12 +1,14 @@
 
 
 def main():
-    wybor = input("Wybierz jedna z opcji: [1,2]:")
+    wybor = input("Wybierz jedna z opcji: [1,2,3]:")
     print('Wybrales:{0}'.format(wybor))
 
     if wybor == '1':
         print('Opcja 1')
     elif wybor == '2':
         print('Opcja 2')
+    elif wybor=='3':
+        print('Wybrales najlepsza opcje')
 
 main()
